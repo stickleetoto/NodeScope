753fb8deb89c8485fc95441d0a2c23e5a81b3d90
stockman <stickee090@gmail.com>
2026-09-22 09:45:32 +0900

    refactor: update NodeScope module imports

--- internal/history/compaction.go
+++ internal/history/compaction.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jjp-monitor/jjp/internal/atomicfile"
+	"github.com/stickleetoto/NodeScope/internal/atomicfile"
 )
 
 func (s *Store) Compact(now time.Time) error {
